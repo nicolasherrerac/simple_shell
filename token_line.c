5d380cd6b372a9972b42715c6e05fedcd4d432d6
Nicolas <nicolasherrerac20@gmail.com>
2020-11-22 16:12:37 +0000

    Add file token_line.c

---
+++ token_line.c
@@ -0,0 +1,25 @@
+#include "shell.h"
+/**
+ * token_line - Function that token args.
+ * @line: command line.
+ * Return: args.
+ */
+char **token_line(char *line)
+{
+	int i;
+	char **args = malloc(sizeof(char *) * 5);  /*Error 3*/
+
+	if (args == NULL)
+	{
+		free(args);
+		exit(1);
+	}
+	i = 0;
+	args[i] = strtok(line, "\n ");
+	while (args[i] != NULL)
+	{
+		i++;
+		args[i] = strtok(NULL, "\n ");
+	}
+	return (args);
+}
